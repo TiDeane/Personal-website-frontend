5c95d3ce8ed4ae5a759c4190e5b46a194797c040
Tiago Deane Motta e Silva <tiagodeane@tecnico.ulisboa.pt>
2025-07-02 17:50:35 +0100

    feat: implement top countries GET info

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,10 +1,16 @@
-/* Call API Gateway to increment visitor count by country */
-fetch('https://ttkp7mg2o7.execute-api.eu-west-1.amazonaws.com/track', { method: 'POST' })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    document.getElementById('visitor-message').textContent = data.message;
-  }).catch(err => console.error('Failed to update country count:', err));
+/* Increment visitor count by country */
+fetch('https://ttkp7mg2o7.execute-api.eu-west-1.amazonaws.com/track', {
+  method: 'POST'
+}).catch(err =>
+  console.error('Failed to update country count:', err)
+);
 
 /* Fetch top visitors by country */
-//TODO
+fetch('https://ttkp7mg2o7.execute-api.eu-west-1.amazonaws.com/top')
+  .then(res => res.json())
+  .then(data => {
+    const countries = data.topCountries.map(c => `${c.countryCode} (${c.count})`);
+    document.getElementById('visitor-message').textContent = `Top countries by visitor count: ${countries.join(', ')}`;
+  }).catch(() =>
+    document.getElementById('visitor-message').textContent = ''
+  );
