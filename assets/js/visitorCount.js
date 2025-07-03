3edf99a67ecd7ebb953a453efae435b803230824
Tiago Deane Motta e Silva <tiagodeane@tecnico.ulisboa.pt>
2025-07-03 14:22:19 +0100

    feat: update README

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,12 +1,12 @@
 /* Increment visitor count by country */
-fetch('https://${CLOUDFRONT_DNS}.cloudfront.net/track', {
+fetch('https://d3f2t5moya606c.cloudfront.net/track', {
   method: 'POST'
 }).catch(err =>
   console.error('Failed to update country count:', err)
 );
 
 /* Fetch top visitors by country */
-fetch('https://${HTTP_API_ID}.execute-api.${AWS_REGION}.amazonaws.com/top')
+fetch('https://epgcnl1cpf.execute-api.eu-west-1.amazonaws.com/top')
   .then(res => res.json())
   .then(data => {
     const countries = data.topCountries.map(c => `${c.countryName} (${c.count})`);
