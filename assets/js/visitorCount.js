1924a16c48ea2436071215cd8fb926208bdaaf6e
Tiago Deane Motta e Silva <tiagodeane@tecnico.ulisboa.pt>
2025-07-03 13:08:30 +0100

    fix: make workflow invalidate CloudFront cache, use environment variables

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,15 +1,15 @@
 /* Increment visitor count by country */
-fetch('https://d3f2t5moya606c.cloudfront.net/track', {
+fetch('https://${CLOUDFRONT_DNS}.cloudfront.net/track', {
   method: 'POST'
 }).catch(err =>
   console.error('Failed to update country count:', err)
 );
 
 /* Fetch top visitors by country */
-fetch('https://ttkp7mg2o7.execute-api.eu-west-1.amazonaws.com/top')
+fetch('https://${HTTP_API_ID}.execute-api.${AWS_REGION}.amazonaws.com/top')
   .then(res => res.json())
   .then(data => {
-    const countries = data.topCountries.map(c => `${c.countryCode} (${c.count})`);
+    const countries = data.topCountries.map(c => `${c.countryName} (${c.count})`);
     document.getElementById('visitor-message').textContent = `Top countries by visitor count: ${countries.join(', ')}`;
   }).catch(() =>
     document.getElementById('visitor-message').textContent = ''
